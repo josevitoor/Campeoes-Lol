f3b42a084c15149fc70db0a68ddb378eae0098ac
josevitoor <josevitor_12@live.com>
2021-04-20 23:16:19 -0300

    create index

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function startFavorite() {
 
 function addFavorite(favorite) {
     localStorage.setItem(favorite, favorite);
-    location.href = "/src/favorite.html";
+    location.href = "/favorite.html";
 }
 
 function removeFavorite(favorite) {
